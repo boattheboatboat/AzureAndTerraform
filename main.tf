8fb3a5ed7172c6f7d066b2bfc64df48d2d52802c
boattheboatboat <55654869+boattheboatboat@users.noreply.github.com>
2022-05-07 15:58:43 +0200

    Added Storage Account, SQL Server and SQL DB

--- main.tf
+++ main.tf
@@ -14,6 +14,39 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "RG-Azure-SQL-01" {
-  name = "U-SC-RG-Azure-SQL-01"
-  location = "Sweden Central"
+  name     = "U-SC-RG-Azure-SQL-01"
+  location = var.UAT-Azure-Location
+  tags     = var.UAT-Azure-Tags
+}
+
+resource "azurerm_storage_account" "SA-Azure-SQL-01" {
+  name                     = "uscsaazuresql01"
+  resource_group_name      = azurerm_resource_group.RG-Azure-SQL-01.id
+  location                 = var.UAT-Azure-Location
+  account_tier             = var.UAT-Azure-Account_tier
+  account_replication_type = var.UAT-Azure-account_replication_type
+  tags                     = var.UAT-Azure-Tags
+}
+
+resource "azurerm_mssql_server" "SQLSRV-Azure-SQL-01" {
+  name                         = "u-sc-sql-srv-01"
+  resource_group_name          = azurerm_resource_group.RG-Azure-SQL-01.id
+  location                     = var.UAT-Azure-Location
+  version                      = "12.0"
+  administrator_login          = "dbajimhil"
+  administrator_login_password = "MyV3ryVERYs3cUREp4$$w0rD"
+  tags                         = var.UAT-Azure-Tags
+}
+
+resource "azurerm_mssql_database" "SQlDB-Azure-SQL-01" {
+  name           = "U-SC-DB-01"
+  server_id      = azurerm_mssql_server.SQLSRV-Azure-SQL-01.id
+  collation      = var.UAT-Azure-Collation
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  sample_name    = "AdventureWorksLT"
+  zone_redundant = true
+  tags           = var.UAT-Azure-Tags
 }
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "UAT-Azure-Tags" {
+  type        = map(any)
+  description = "This sets the correct tags for Azure Resources in UAT"
+  default = {
+    Environment = "UAT"
+    Cloud       = "Azure"
+    Terraform   = "True"
+  }
+}
+
+variable "UAT-Azure-Location" {
+  type        = string
+  description = "Sets the Location to Sweden Central"
+  default     = "Sweden Central"
+}
+
+variable "UAT-Azure-Account_tier" {
+  type        = string
+  description = "Sets the account tier for Storage Accounts in UAT"
+  default     = "Standard"
+}
+
+variable "UAT-Azure-account_replication_type" {
+  type        = string
+  description = "Sets the account replication type in UAT"
+  default     = "LRS"
+}
+
+variable "UAT-Azure-Collation" {
+  type        = string
+  description = "Sets the Collation of Databases"
+  default     = "SQL_Latin1_General_CP1_CI_AS"
+}
